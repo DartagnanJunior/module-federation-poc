c56dc2297b85cfa46e1d1bc84adaeaaa8d5844a9
A. Dartagnan <dartajunior3@gmail.com>
2025-05-23 16:51:34 -0300

    refactor: update environment variable names and streamline currency analysis setup

--- projects/currency-analysis/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  remoteEntry: 'http://localhost:4201/remoteEntry.js'
-};
--- projects/plataform/webpack.config.js
+++ projects/plataform/webpack.config.js
@@ -1,11 +1,19 @@
+const path = require("path");
 const {
   shareAll,
   withModuleFederationPlugin,
 } = require("@angular-architects/module-federation/webpack");
 
+require("dotenv").config({
+  path: path.resolve(
+    __dirname,
+    "../../env/.env." + (process.env.NODE_ENV || "development")
+  ),
+});
+
 module.exports = withModuleFederationPlugin({
   remotes: {
-    currencyAnalysis: "http://localhost:4201/remoteEntry.js",
+    currencyAnalysis: process.env.CURRENCY_REMOTE_URL,
   },
 
   shared: {
